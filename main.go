8216174d7c218abb4ff9728b656e2d81a48e7537
Anas Firly <anasfirly20@gmail.com>
2023-10-09 13:57:41 +0700

    learning pointer

--- main.go
+++ main.go
@@ -23,5 +23,15 @@ func main() {
 		},
 	}
 
-	fmt.Printf("%+v", jim)
+	jimPointer := &jim
+	jimPointer.updateName("Jimmy")
+	jim.print()
+}
+
+func (p person) print(){
+	fmt.Printf("%+v", p)
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
 }
